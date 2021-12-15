debeabf9ced62f9389f83c7f5c9875d2629d4ffc
mlm-25 <youngmauri43@gmail.com>
2021-12-14 21:22:02 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Dog");
+            Thread.sleep(2000);
+        }
+    }
+}
